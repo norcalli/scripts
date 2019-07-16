5d9ab8e43a8b891169092f247b56c8161dd6f12c
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2019-07-16 16:51:01 -0700

    Update auto readme to print help text as well.
    - `timeout -sKILL 0.01 $SCRIPT --help` to generate help text.
    - Use h5 to format script sections
    - Change parser

--- README.md
+++ README.md
@@ -10,7 +10,19 @@
 #### Some scripts. That's it.
 
 #### Descriptions:
-- `pbdo`: Runs actions based on your clipboard using pbpaste
-- `pastie`: Posts a file to pastie from the command line.
-- `auto_readme.py`: Automatically generates the descriptions in this README.md by parsing scripts for "Description: *" line
-- `nvas`: Opens the filenames provided in the args inside of a **running** Neovim by detecting $NVIM_LISTENER_ADDR. This is useful for the new :terminal mode, so that you can open a file in the current neovim without leaving terminal mode. It also waits for the buffer to be closed/detached before exiting, which allows it to be used for commands which call to `$EDITOR` for input, such as `git commit -v`.
+##### `pastie`
+Posts a file to pastie from the command line.
+
+##### `pbdo`
+Runs actions based on your clipboard using pbpaste
+
+##### `yay-update-ignore-errors`
+Wrapper script around yay to try to ignore errors.
+```sh
+Usage: ./yay-update-ignore-errors <package-to-ignore...>
+
+```
+
+##### `auto_readme.py`
+Automatically generates the descriptions in this README.md by parsing scripts for "Description: *" line
+
--- auto_readme.py
+++ auto_readme.py
@@ -1,82 +1,130 @@
 #!/usr/bin/env python3
-import os
-import sys
-import re
+# vim:ts=4 noet sw=4 cc=80
 # Author: Ashkan Kiani
 # Version: 0.1
 # Description: Automatically generates the descriptions in this README.md by parsing scripts for "Description: *" line
-def formatDescription(name, description):
-  return "- `{}`: {}".format(name, description.strip())
+
+import os
+import sys
+import re
+
+TITLE_HEADER_LEVEL = "####"
+SCRIPT_HEADER_LEVEL = TITLE_HEADER_LEVEL+"#"
+
+
+def format_description(name, description, help_text):
+		if help_text is not None:
+				# return "- `{}`: {}\n	- ```\n{}\n```".format(name, description.strip(), help_text)
+				return SCRIPT_HEADER_LEVEL+" `{}`\n{}\n```sh\n{}\n```\n".format(name, description.strip(), help_text)
+		else:
+				return SCRIPT_HEADER_LEVEL+" `{}`\n{}\n".format(name, description.strip())
 
 # TODO: Support version replacement?
-# lineRegex = re.compile('- `([^`]+)`-([0-9.]+)?:(.*)')
-lineRegex = re.compile('- `([^`]+)`:(.*)')
-def parseLine(line):
-  """
-  >>> parseLine('- `pbdo`:')
-  ('pbdo', '')
-  >>> parseLine('- `puppet`: A glorious plugin')
-  ('puppet', ' A glorious plugin')
-  """
-  try:
-    return lineRegex.findall(line)[0]
-  except IndexError:
-    print(line, file=sys.stderr)
-    return ('', '')
+
+# Run the script to generate help text.
+# However, if I didn't actually implement a --help flag case,
+# then kill it just in case it starts to do things in as little
+# time as possible. It turns out that takes about 0.005-0.01 seconds
+# for a simple bash script.
+def get_help_text(script_file_name):
+		import subprocess
+		result = subprocess.run(["timeout", "-sKILL", "0.01", script_file_name, "--help"], capture_output=True)
+		help_text = result.stderr.decode("utf-8")
+		if not help_text.lower().startswith("usage:"):
+				print(script_file_name, "invalid help text, skipping.", help_text)
+				return None
+		return help_text
+
+SCRIPT_NAME_REGEX = re.compile(SCRIPT_HEADER_LEVEL+' `([^`]+)`')
+
+def parse_descriptions(lines):
+	description_dictionary = {}
+	while len(lines) > 0:
+		line = lines.pop()
+		if line.strip() == '':
+			continue
+		# Parse the lines of existing descriptions, which may include
+		# manually added descriptions
+		try:
+			script_name = SCRIPT_NAME_REGEX.findall(line)[0]
+			line = lines.pop()
+			script_description = []
+			while not line.startswith("```") and not line.startswith(SCRIPT_HEADER_LEVEL+" "):
+				script_description.push(line)
+				try:
+					line = lines.pop()
+				except IndexError:
+					break
+			description_dictionary[script_name] = script_description
+		except IndexError:
+			pass
+		return description_dictionary
 
 
-def UpdateDescriptions(d):
-  lines = []
-  with open('./README.md') as file:
-    lines = [line.rstrip() for line in file.readlines()]
-  idx = 0
-  for line in lines:
-    if 'Descriptions' in line:
-      break
-    idx += 1
-  header = lines[:idx+1]
-  descriptions = lines[idx+1:]
-  descDict = {}
-  for description in descriptions:
-    if description.strip() == '':
-      continue
-    info = parseLine(description)
-    descDict[info[0]] = info[1]
-  descDict.update(d)
-  descriptions = [formatDescription(name, description) for name, description in descDict.items()]
-  print("Descriptions:", file=sys.stderr)
-  for name, desc in descDict.items():
-    print("{}: {}".format(name, desc), file=sys.stderr)
-  print("", file=sys.stderr)
-  with open('./README.md', 'w') as file:
-    file.writelines(line + "\n" for line in header + descriptions)
+# Parse the existing README and find descriptions
+# Update them with new descriptions from the passed in
+# dictionary. Leave anything before the line "#### Descriptions"
+def update_descriptions(new_descriptions):
+	lines = []
+	with open('./README.md') as file:
+		lines = [line.rstrip() for line in file.readlines()]
+	idx = 0
+	for line in lines:
+		if line.startswith(TITLE_HEADER_LEVEL+' Descriptions'):
+			break
+		idx += 1
+	# Up to and including "#### Descriptions"
+	header = lines[:idx+1]
 
-descriptionRegex = re.compile('Description:(.*)')
-def FindDescriptions(files):
-  result = {}
-  for filename in files:
-    with open(filename) as file:
-      lines = file.readlines()
-      description = None
-      for line in lines:
-        match = descriptionRegex.findall(line)
-        if len(match) > 0:
-          description = match[0]
-          break
-      if description is None:
-        continue
-      result[filename] = description.rstrip()
-  return result
+	# Everything after.
+	description_dictionary = parse_descriptions(lines[idx+1:])
+
+	description_dictionary.update(new_descriptions)
+
+	descriptions = [
+		format_description(
+			name,
+			description,
+			get_help_text("./" + name),
+		)
+		for name, description in description_dictionary.items()]
+
+	sys.stderr.writelines(line + "\n" for line in header + descriptions)
+
+	with open('./README.md', 'w') as file:
+		file.writelines(line + "\n" for line in header + descriptions)
+
+
+description_regex = re.compile('Description:(.*)')
+
+
+def find_descriptions(files):
+	result = {}
+	for filename in files:
+		with open(filename) as file:
+			lines = file.readlines()
+			description = None
+			for line in lines:
+				match = description_regex.findall(line)
+				if len(match) > 0:
+					description = match[0]
+					break
+			if description is None:
+				continue
+			result[filename] = description.rstrip()
+	return result
+
 
 # TODO: Auto parse scripts for Description
 def main():
-  blacklist = ('^\.', 'personal', 'README.md', '\.swp$', '__pycache__')
-  blacklist = [re.compile(x) for x in blacklist]
-  blacklisted = lambda x: any(b.search(x) for b in blacklist)
-  files = os.listdir()
-  files = [file for file in files if not blacklisted(file)]
-  descDict = FindDescriptions(files)
-  UpdateDescriptions(descDict)
+	blacklist = ('^\.', 'personal', 'README.md', '\.swp$', '__pycache__')
+	blacklist = [re.compile(x) for x in blacklist]
+	blacklisted = lambda x: any(b.search(x) for b in blacklist)
+	files = os.listdir()
+	files = [file for file in files if not blacklisted(file)]
+	new_descriptions = find_descriptions(files)
+	update_descriptions(new_descriptions)
+
 
 if __name__ == '__main__':
-  main()
+	main()
